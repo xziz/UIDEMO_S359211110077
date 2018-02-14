0023edd810605ff4c669b7303a260d8e78cabd6f
xziz <aziz0822>
2018-02-14 10:17:03 +0700

    dbConnection

---
+++ src/dbUtil/dbConnection.java
@@ -0,0 +1,22 @@
+package dbUtil;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+    private static final String SQCONN = "jdbc:school.sqlite";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(SQCONN);
+
+        }catch (ClassNotFoundException ex){
+            ex.printStackTrace();
+        }
+        return null;
+    } //getConnection
+
+} //class
---
+++ src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import com.sun.org.apache.xpath.internal.functions.FuncFalse;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("School Management System");
+        primaryStage.setScene(new Scene(root, 600, 390));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
