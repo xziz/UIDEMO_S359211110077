ef23f75154af3880aff49e02eb7ed628f0a64913
xziz <aziz0822>
2018-02-14 10:42:44 +0700

    Main

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConnection {
-    private static final String SQCONN = "jdbc:school.sqlite";
+    private static final String SQCONN = "jdbc:sqlite:school.sqlite";
 
     public static Connection getConnection() throws SQLException {
         try {
